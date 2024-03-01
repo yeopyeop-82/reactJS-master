fabc5278b3fdb45d9bfd3ac9424e811aaca30989
yeopyeop-82 <a01055143647@gmail.com>
2024-03-01 23:14:49 +0900

    #6.6

--- todo_list/src/ToDoList.tsx
+++ todo_list/src/ToDoList.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function ToDoList() {
   const [todo, setTodo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
+    setToDoError("");
     setTodo(value);
   };
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (todo.length < 10) {
+      return setToDoError("To do should be longer than 10 letters");
+    }
     console.log(todo);
   };
 
@@ -18,6 +24,23 @@ function ToDoList() {
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={todo} placeholder="Write a to do" />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+} */
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  console.log(watch());
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("password")} placeholder="Password" />
         <button>Add</button>
       </form>
     </div>
