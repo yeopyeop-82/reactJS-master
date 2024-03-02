f357eb324e088fa2b2f19f1778b380222e48d1a3
yeopyeop-82 <a01055143647@gmail.com>
2024-03-02 17:55:05 +0900

    #7.9

--- trello_clone/src/App.tsx
+++ trello_clone/src/App.tsx
@@ -1,13 +1,12 @@
-import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
-import DraggableCard from "./components/DraggableCard";
+import Board from "./components/Board";
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
-  width: 100%;
+  width: 100vw;
   margin: 0 auto;
   justify-content: center;
   align-items: center;
@@ -15,54 +14,54 @@ const Wrapper = styled.div`
 `;
 
 const Boards = styled.div`
-  display: grid;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
   width: 100%;
-  grid-template-columns: repeat(3, 1fr);
-`;
-
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
+  gap: 10px;
 `;
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
 
-  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-    if (!destination) return;
+  const onDragEnd = (info: DropResult) => {
+    console.log(info);
+    const { destination, draggableId, source } = info;
+    if (destination?.droppableId === source.droppableId) {
+      // same board movement.
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        boardCopy.splice(source.index, 1);
+        boardCopy.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
 
-    setToDos((oldToDos) => {
-      const copyToDos = [...oldToDos];
-      // 1) delete item on source.index
-      console.log("Delete item on ", source.index);
-      console.log(copyToDos);
-      copyToDos.splice(source.index, 1);
-      console.log("Delete item ");
-      // 2) Put back the item on the destiantion.index
-      console.log("put back ", draggableId, "on ", destination.index);
-      copyToDos.splice(destination?.index, 0, draggableId);
-      console.log(copyToDos);
-      return copyToDos;
-    });
+    // setToDos((oldToDos) => {
+    //   const copyToDos = [...oldToDos];
+    //   // 1) delete item on source.index
+    //   console.log("Delete item on ", source.index);
+    //   console.log(copyToDos);
+    //   copyToDos.splice(source.index, 1);
+    //   console.log("Delete item ");
+    //   // 2) Put back the item on the destiantion.index
+    //   console.log("put back ", draggableId, "on ", destination.index);
+    //   copyToDos.splice(destination?.index, 0, draggableId);
+    //   console.log(copyToDos);
+    //   return copyToDos;
+    // });
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DraggableCard key={toDo} index={index} toDo={toDo} />
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
--- trello_clone/src/components/DraggableCard.tsx
+++ trello_clone/src/components/DraggableCard.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 const Card = styled.div`
   background-color: ${(props) => props.theme.cardColor};
   border-radius: 5px;
+  margin-bottom: 5px;
   padding: 10px 10px;
 `;
 
