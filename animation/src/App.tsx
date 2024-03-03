7f0d70185d48004fcf331cdeaee3ebd2fdb1a725
yeopyeop-82 <a01055143647@gmail.com>
2024-03-03 23:15:52 +0900

    #8.9

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,32 +1,57 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
+import { useEffect } from "react";
+import { linear } from "popmotion";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 500vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238)) 0%, rgb(0, 83, 238) 100%)",
+      "linear-gradient(135deg, rgb(238, 0, 153)) 0%, rgb(221, 0, 238) 100%)",
+      "linear-gradient(135deg, rgb(0, 238, 155)) 0%, rgb(0, 83, 238) 100%)",
+    ]
+  );
+
+  const { scrollYProgress } = useViewportScroll();
+
+  const scaleSize = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
   return (
-    <Wrapper>
+    <Wrapper style={{ background: gradient }}>
       <Box
-        transition={{
-          type: "spring",
-          bounce: 0.5,
+        style={{
+          x,
+          rotate: scale,
+          scale: scaleSize,
         }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
+        drag="x"
+        dragSnapToOrigin
       />
     </Wrapper>
   );
